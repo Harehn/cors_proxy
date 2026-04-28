8c6ad6d811b51704f4d0d3348cd13f9e5e3adeef
Nitin <harehn.kaundun@mail.mcgill.ca>
2026-04-28 17:52:18 -0400

    Full form code

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(cors()); // allow your frontend
 
 app.get("/api/proxy", async (req, res) => {
   try {
-	res.send("Hello World");
-	//res.send(process.env.API_URL);
-    //const response = await fetch(process.env.API_URL);
-    //const data = await response.text(); // or .json()
+	//res.send("Hello World");
+	res.send(process.env.API_URL);
+    const response = await fetch(process.env.API_URL);
+    const data = await response.text(); // or .json()
 
-    //res.send(data);
+    res.send(data);
   } catch (err) {
     res.status(500).json({ error: "Proxy failed" });
   }
