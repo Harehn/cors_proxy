9eab3779dfa0ad3747dd9d0ec4f982da4320af16
Nitin <harehn.kaundun@mail.mcgill.ca>
2026-04-28 17:41:22 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,23 @@
+// server.js
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors()); // allow your frontend
+
+app.get("/api/proxy", async (req, res) => {
+  try {
+	//res.send("Hello World");
+	res.send(process.env.API_URL);
+    //const response = await fetch(process.env.API_URL);
+    //const data = await response.text(); // or .json()
+
+    //res.send(data);
+  } catch (err) {
+    res.status(500).json({ error: "Proxy failed" });
+  }
+});
+
+app.listen(10000, () => console.log("Running"));
